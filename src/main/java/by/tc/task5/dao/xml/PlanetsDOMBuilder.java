174805a4e636cfeef701a77ee35391a742bd87b2
Hleb_Aksionau <Hleb_Aksionau@epam.com>
2018-03-20 13:35:23 +0300

    exceptions2

--- src/main/java/by/tc/task5/dao/xml/PlanetsDOMBuilder.java
+++ src/main/java/by/tc/task5/dao/xml/PlanetsDOMBuilder.java
@@ -1,7 +1,10 @@
 package by.tc.task5.dao.xml;
 
 import by.tc.task5.entity.Planet;
-import org.w3c.dom.*;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -30,43 +33,35 @@ public class PlanetsDOMBuilder {
         return planets;
     }
 
-    public void buildPlanets(String fileName) {
-        Document doc = null;
-
-        try {
-            doc=documentBuilder.parse(fileName);
-            Element root=doc.getDocumentElement();
-            NodeList planetsList=root.getElementsByTagName("planet");
-            int length=planetsList.getLength();
-            for (int i=0; i<length; i++){
-                Element planetElement=(Element)planetsList.item(i);
-                if (planetElement.getNodeType() == Node.ELEMENT_NODE) {
-                    Planet planet = buildPlanet(planetElement);
-                    planets.add(planet);
-                }
+    public void buildPlanets(String fileName) throws IOException, SAXException {
+        Document doc;
+        doc = documentBuilder.parse(fileName);
+        Element root = doc.getDocumentElement();
+        NodeList planetsList = root.getElementsByTagName("planet");
+        int length = planetsList.getLength();
+        for (int i = 0; i < length; i++) {
+            Element planetElement = (Element) planetsList.item(i);
+            if (planetElement.getNodeType() == Node.ELEMENT_NODE) {
+                Planet planet = buildPlanet(planetElement);
+                planets.add(planet);
             }
 
-        } catch (SAXException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-
 
     }
 
-    private Planet buildPlanet(Element planetElement){
-        Planet planet=new Planet();
-        String nodeValue=planetElement.getElementsByTagName("diameter").item(0).getTextContent();
-        Integer diam=Integer.parseInt(nodeValue);
+    private Planet buildPlanet(Element planetElement) {
+        Planet planet = new Planet();
+        String nodeValue = planetElement.getElementsByTagName("diameter").item(0).getTextContent();
+        Integer diam = Integer.parseInt(nodeValue);
         planet.setDiameter(diam);
-        nodeValue=planetElement.getElementsByTagName("name").item(0).getTextContent();
+        nodeValue = planetElement.getElementsByTagName("name").item(0).getTextContent();
         planet.setName(nodeValue);
-        nodeValue=planetElement.getElementsByTagName("orbitalPeriod").item(0).getTextContent();
-        Integer arbPer=Integer.parseInt(nodeValue);
+        nodeValue = planetElement.getElementsByTagName("orbitalPeriod").item(0).getTextContent();
+        Integer arbPer = Integer.parseInt(nodeValue);
         planet.setOrbitalPeriod(arbPer);
-        nodeValue=planetElement.getElementsByTagName("yearOfDiscovering").item(0).getTextContent();
-        Integer year=Integer.parseInt(nodeValue);
+        nodeValue = planetElement.getElementsByTagName("yearOfDiscovering").item(0).getTextContent();
+        Integer year = Integer.parseInt(nodeValue);
         planet.setYearOfDiscovering(year);
         return planet;
     }
