11742e04d02d7a33a6f55c02fa2614e7372b7603
Hleb_Aksionau <Hleb_Aksionau@epam.com>
2018-03-21 12:55:43 +0300

    relative path

--- src/main/java/by/tc/task5/controller/PageController.java
+++ src/main/java/by/tc/task5/controller/PageController.java
@@ -1,25 +1,37 @@
 package by.tc.task5.controller;
 
+import by.tc.task5.dao.XmlParserType;
 import by.tc.task5.entity.Planet;
 import by.tc.task5.service.XmlDataService;
 import by.tc.task5.service.XmlDataServiceImpl;
 import org.xml.sax.SAXException;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.List;
 
 public class PageController extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        XmlDataService service = new XmlDataServiceImpl("d:\\Java\\projects\\IDEA\\Task_5_Web_application_with_pagination\\data\\planets.xml");
+        ServletContext context = getServletContext();
+        String pathToXML = context.getRealPath("/WEB-INF/resources/planets.xml");
+        XmlDataService service = new XmlDataServiceImpl(pathToXML);
         List<Planet> planets = null;
+
+        HttpSession session = req.getSession(true);
+        if ((session.getAttribute("xmlParserType") == null) || req.getParameter("xmlParserType") != null) {
+            session.setAttribute("xmlParserType", req.getParameter("xmlParserType"));
+        }
         try {
-            planets = service.getPlanetsByDOMParser();
+            XmlParserType parserType = XmlParserType.valueOf((String) session.getAttribute("xmlParserType"));
+            planets = service.getPlanets(parserType);
         } catch (SAXException e) {
 
         }
